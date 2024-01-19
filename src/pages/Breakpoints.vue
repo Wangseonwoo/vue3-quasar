5271ad843fdd0757f4257e7390bcd94c187c4c51
Wangseonwoo <dmb05007@naver.com>
2024-01-19 18:27:18 +0900

    BreakPoints.1

---
+++ src/pages/Breakpoints.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-xl">
+    <section class="q-mb-xl">
+      <div class="text-h4">Breakpoints</div>
+      <q-separator class="q-my-md" />
+      <div class="target"></div>
+    </section>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.target {
+  height: 200px;
+  background-color: $dark;
+}
+@media (max-width: $breakpoint-xs-max) {
+  .target {
+    background-color: $red;
+  }
+}
+</style>
